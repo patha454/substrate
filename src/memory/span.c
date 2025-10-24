b151af1691ebb48cf7d6d72d0df72e4c0e00c00b
H Paterson <26947190+patha454@users.noreply.github.com>
2025-10-24 22:16:01 +1100

    Feature: Implement naieve `sbCopySpan()`

--- src/memory/span.c
+++ src/memory/span.c
@@ -13,5 +13,28 @@ extern SbSpan sbCreateSpan(void * ptr, size_t len) {
 }
 
 extern bool sbCopySpan(SbSpan origin, SbSpan destination) {
-    return false;
+    if (origin.len == 0 || destination.len == 0) {
+        return true;
+    }
+    const size_t copyLen =
+        origin.len < destination.len ? origin.len : destination.len;
+    /*
+     * The copy must be done in the reverse order if the end of the origin
+     * overlaps with the start of the destination.
+     */
+    bool reverse = destination.ptr > origin.ptr &&
+        destination.ptr < origin.ptr + copyLen;
+
+    if (reverse) {
+        for (size_t i = copyLen; i > 0; i--) {
+            ((uint8_t *) destination.ptr)[i - 1] = ((uint8_t *) origin.ptr)[i - 1];
+        }
+        return true;
+    }
+
+    for (size_t i = 0; i < copyLen; i++) {
+        ((uint8_t *) destination.ptr)[i] = ((uint8_t *) origin.ptr)[i];
+    }
+
+    return true;
 }
